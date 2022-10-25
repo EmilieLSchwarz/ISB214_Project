9548b98012e9dc138e8e6efb75bec67bc360891c
EmilieLSchwarz <98109404+EmilieLSchwarz@users.noreply.github.com>
2022-10-25 17:11:37 +0200

    ADDING FUNCTIONS FOR GROUPS 1-4

--- inca_initial_exploration.R
+++ inca_initial_exploration.R
@@ -18,6 +18,39 @@ catvars <- (c("diet","disease", "season", "smoking_status", "age_categories", "i
 
 inca2[, catvars] <- lapply(inca2[, catvars], factor)
 
+# making group 1 (average of group 1)
+group1_fun <- function(data){
+  for(i in 1:nrow(data)){
+    data[i,"grp1"]= (data$food_gp_1s[i]+data$food_gp_3s[i]+data$food_gp_4s[i]+data$food_gp_5s[i])/(4)
+    }
+return(data$grp1)
+  }
+inca2$grp1 <- group1_fun(inca2)
 
+# making group 2
+group2_fun <- function(data){
+  for(i in 1:nrow(data)){
+    data[i,"grp2"]= (data$food_gp_2s[i]+data$food_gp_6s[i]+data$food_gp_7s[i]+data$food_gp_8s[i]+data$food_gp_28s[i]+data$food_gp_29s[i]+data$food_gp_30s[i]+data$food_gp_41s[i]+data$food_gp_42s[i]+data$food_gp_43s[i])/(10)
+  }
+  return(data$grp2)
+}
+inca2$grp2 <- group2_fun(inca2)
 
+#making group 3 
+group3_fun <- function(data){
+  for(i in 1:nrow(data)){
+    data[i,"grp3"]= (data$food_gp_9s[i]+data$food_gp_10s[i]+data$food_gp_11s[i])/(3)
+  }
+  return(data$grp3)
+}
+inca2$grp3 <- group3_fun(inca2)
 
+#making group 4 
+
+group4_fun <- function(data){
+  for(i in 1:nrow(data)){
+    data[i,"grp4"]= (data$food_gp_12s[i]+data$food_gp_17s[i]+data$food_gp_18s[i]+data$food_gp_19s[i]+data$food_gp_21s[i]+data$food_gp_22s[i])/(6)
+  }
+  return(data$grp4)
+}
+inca2$grp4 <- group4_fun(inca2)
